464b6c945cc126cb9e70bb2ad5b6b8aa3f175791
Jakub Ransdorf <74991147+kubaran11@users.noreply.github.com>
2024-03-25 10:05:40 +0100

    Add files via upload

---
+++ src/main/java/org/example/GameLogic.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.example.logic.Ball;
+import org.example.logic.Direction;
+
+import java.awt.*;
+
+public class GameLogic {
+    private Ball ball;
+    private Ball ballEnemy;
+
+    public GameLogic() {
+        this.ball = null;
+    }
+
+    public void initialize() {
+        ball = new Ball(20, 20, 50, 50, Color.BLUE);
+    }
+
+    public void update() {
+        ball.move(2, Direction.RIGHT);
+    }
+
+    public Ball getBall() {
+        return ball;
+    }
+
+}
